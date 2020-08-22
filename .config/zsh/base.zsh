675a5a929a6963115859fb9d8e9739e1d4737b27
Glenn Waters <gwwaters@gmail.com>
2020-08-21 22:53:28 -0400

    Move poetry into .local

--- .config/zsh/base.zsh
+++ .config/zsh/base.zsh
@@ -2,20 +2,6 @@
 # All things interactive shell
 #
 
-# Add to the path ensuring no duplicates
-for x in ~/bin ~/.poetry/bin /usr/local/opt/ruby/bin /usr/local/bin; do
-  case ":$PATH:" in
-    *":$x:"*) :;; # already there
-    *) PATH="$x:$PATH";;
-  esac
-done
-
-export PYENV_ROOT=~/.local/share/pyenv
-
-# If pyenv shims not already in path, add them
-[[ ":$PATH:" != *":$PYENV_ROOT/shims:"* ]] && eval "$(pyenv init -)"
-true
-
 export LANG="en_CA.UTF-8"
 export LC_ALL="en_CA.UTF-8"
 
@@ -26,8 +12,8 @@ export CUPS_DATADIR=~/.local/share/cups
 export FZF_DEFAULT_COMMAND='rg --files --hidden --follow --no-messages --glob "!.git/*"'
 export FZF_DEFAULT_OPTS="--inline-info --color fg:-1,bg:-1,hl:45,fg+:3,bg+:233,hl+:229,info:150,prompt:110,spinner:150,pointer:167,marker:174"
 export PYLINTHOME=~/.local/share/pylint
-
 export PIP_CONFIG_FILE=~/.config/pip/pip.conf
+export POETRY_HOME=~/.local/poetry
 export RIPGREP_CONFIG_PATH=~/.config/ripgrep/ripgreprc
 export LESS="-RM"
 export LESSHISTFILE=-
@@ -43,6 +29,20 @@ export EXA_COLORS='da=38;5;12:di=38;5;51'
 export ELKM1_URL=elk://192.168.1.201
 export UPBPIM_URL=serial:///dev/cu.KeySerial1:4800
 export UPBPIM_URL=tcp://192.168.1.14:7000
+#
+# Add to the path ensuring no duplicates
+for x in ~/bin $POETRY_HOME/bin /usr/local/opt/ruby/bin /usr/local/bin; do
+  case ":$PATH:" in
+    *":$x:"*) :;; # already there
+    *) PATH="$x:$PATH";;
+  esac
+done
+
+export PYENV_ROOT=~/.local/share/pyenv
+
+# If pyenv shims not already in path, add them
+[[ ":$PATH:" != *":$PYENV_ROOT/shims:"* ]] && eval "$(pyenv init -)"
+true
 
 # Setup for good long ability to look back on life...
 export HISTFILE=~/.local/share/zsh/history

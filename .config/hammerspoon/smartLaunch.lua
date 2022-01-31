b6f980c11be11ad90241c6aafc8cd7e61f784c7f
Glenn Waters <gwwaters@gmail.com>
2022-01-31 18:53:06 -0500

    Fix local var usage.

--- .config/hammerspoon/smartLaunch.lua
+++ .config/hammerspoon/smartLaunch.lua
@@ -1,15 +1,15 @@
 -- Roughly inspired by: https://github.com/szymonkaliski/dotfiles
 
-local module = {}
-local savedState = {}
+local m = {}
+m._savedState = {}
 
-module.init = function(hyper)
-  hyper.addHook(nil, function() savedState = {} end)
+m.init = function(hyper)
+  hyper.addHook(nil, function() m._savedState = {} end)
 end
 
-module.smartLaunch = function(launchApp, name)
-  if savedState.lastApp ~= launchApp then
-    savedState = {lastApp = launchApp}
+m.smartLaunch = function(launchApp, name)
+  if m._savedState.lastApp ~= launchApp then
+    m._savedState = {lastApp = launchApp}
   end
 
   if not name then name = launchApp end
@@ -21,24 +21,24 @@ module.smartLaunch = function(launchApp, name)
     hs.application.open(launchApp)
 
   else
-    if not savedState.windows then
-      local wins = hs.fnutils.filter(runningApp:allWindows(), function(win)
+    if not m._savedState.windows then
+      local windows = hs.fnutils.filter(runningApp:allWindows(), function(win)
         return win:isStandard()
       end)
-      savedState.windows = wins
-      savedState.index = hs.fnutils.indexOf(wins, hs.window.frontmostWindow()) or 0
+      m._savedState.windows = windows
+      m._savedState.index = hs.fnutils.indexOf(windows, hs.window.frontmostWindow()) or 0
     end
 
-    if #savedState.windows == 0 then
+    if #m._savedState.windows == 0 then
       hs.alert.show("Creating new " .. name .. " window", 1.2)
       hs.application.open(launchApp)
 
     else
       -- focus a window and set window index for next smartLaunch call
-      savedState.windows[(savedState.index % #savedState.windows)+1]:focus()
-      savedState.index = savedState.index + 1
+      m._savedState.windows[(m._savedState.index % #m._savedState.windows)+1]:focus()
+      m._savedState.index = m._savedState.index + 1
     end
   end
 end
 
-return module
+return m

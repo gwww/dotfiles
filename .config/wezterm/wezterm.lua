7a57996bcd0fae0136c6a1fb8b3aed0919f937b4
Glenn Waters <glenn@watrs.ca>
2022-03-27 22:12:27 -0400

    Tweak colors, rows/cols.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -10,18 +10,18 @@ config.window_decorations = "RESIZE"
 config.swallow_mouse_click_on_pane_focus = true
 config.enable_tab_bar = true
 config.hide_tab_bar_if_only_one_tab = false
-config.initial_cols = 179
-config.initial_rows = 47
-config.initial_cols = 300
-config.initial_rows = 100
+
+config.initial_rows = 50
+config.initial_cols = 180
+
 config.window_padding = {left = 8, right = 2, top = 1, bottom = 1}
+
 config.font_size = 16.5
 config.line_height = 1.05
--- config.harfbuzz_features = {"calt=0", "clig=0", "liga=0"}
 config.font = wt.font("Inconsolata", {weight="Regular"})
 config.warn_about_missing_glyphs = false
+
 config.inactive_pane_hsb = {
-  brightness = 1.0,
   saturation = 0.6,
   brightness = 0.5,
 }
@@ -40,11 +40,11 @@ config.colors = {
     background = "#030303",
     active_tab = {
       fg_color = "#7CFC00",
-      bg_color = "#2b2042",
+      bg_color = "#181818",
     },
     inactive_tab = {
       fg_color = "#a0a0a0",
-      bg_color = "#1b1032",
+      bg_color = "#2b2042",
     },
   },
 }

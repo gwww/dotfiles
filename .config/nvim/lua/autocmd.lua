e68126c69eeb9a1d7ac959a7083a985805a2bfa9
Glenn Waters <glenn@watrs.ca>
2022-04-04 20:36:17 -0400

    Add stylua config.

--- .config/nvim/lua/autocmd.lua
+++ .config/nvim/lua/autocmd.lua
@@ -18,13 +18,7 @@ au2.BufEnter = {
         if vim.bo.buftype == "help" then
             cmd("wincmd L")
             local nr = vim.api.nvim_get_current_buf()
-            vim.api.nvim_buf_set_keymap(
-                nr,
-                "n",
-                "q",
-                ":q<CR>",
-                { noremap = true, silent = true }
-            )
+            vim.api.nvim_buf_set_keymap(nr, "n", "q", ":q<CR>", { noremap = true, silent = true })
         end
     end,
 }

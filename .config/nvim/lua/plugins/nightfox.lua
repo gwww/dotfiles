f4ee20982670a3b2ca306eb05d818c776347472d
Glenn Waters <glenn@watrs.ca>
2022-04-15 22:47:04 -0400

    Tweak color

--- .config/nvim/lua/plugins/nightfox.lua
+++ .config/nvim/lua/plugins/nightfox.lua
@@ -9,7 +9,7 @@ require("nightfox").setup({
     palettes = {
         nightfox = {
             -- bg1 = "#111720",
-            bg1 = Color.from_hsl(216, 31, 7):to_css(),
+            bg1 = Color.from_hsl(216, 31, 7.5):to_css(),
             -- bg3 = "#1b2533",
             bg3 = Color.from_hsl(216, 31, 12):to_css(),
             comment = "#7687a0",

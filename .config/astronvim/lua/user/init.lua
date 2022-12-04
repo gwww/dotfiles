be664725178dd74be96e90d4a16eef4f1dbaf6ae
Glenn Waters <glenn@watrs.ca>
2022-12-04 16:24:14 -0500

    More tweaks

--- .config/astronvim/lua/user/init.lua
+++ .config/astronvim/lua/user/init.lua
@@ -9,7 +9,7 @@ return {
 
     lsp = {
         -- Add to the global LSP on_attach function
-        on_attach = function(client, bufnr) client.config.flags["debounce_text_changes"] = 500 end,
+        on_attach = function(client) client.config.flags["debounce_text_changes"] = 500 end,
     },
 
     mappings = {
@@ -18,6 +18,9 @@ return {
             ["<enter>"] = { ":noh<enter><cr>", silent = true, desc = "No highlight" },
             j = { "v:count==0 ? 'gj' : 'j'", expr = true },
             k = { "v:count==0 ? 'gk' : 'k'", expr = true },
+            ["<leader>e"] = { "<cmd>Telescope find_files<cr>", desc = "Find files" },
+            ["<leader>E"] = { "<cmd>Neotree toggle<cr>", desc = "Toggle NeoTree" },
+            ["<leader>ff"] = false,
         },
     },
 

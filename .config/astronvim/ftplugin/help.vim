992da1ab89c352ef46a275a29186d7b6ddf6010d
Glenn Waters <glenn@watrs.ca>
2022-12-04 14:38:25 -0500

    Add ftplugin and update heirline config.

---
+++ .config/astronvim/ftplugin/help.vim
@@ -0,0 +1,8 @@
+nnoremap <buffer><space> <c-]>
+nnoremap <buffer><BS> <c-T>
+nnoremap <silent><buffer>q :bdelete<CR>
+nnoremap <buffer> o /'\l\{2,\}'<CR>
+nnoremap <buffer> O ?'\l\{2,\}'<CR>
+nnoremap <buffer> s /\|\zs\S\+\ze\|<CR>
+nnoremap <buffer> S ?\|\zs\S\+\ze\|<CR>
+set nonumber
--- .config/astronvim/lua/user/plugins/heirline.lua
+++ .config/astronvim/lua/user/plugins/heirline.lua
@@ -1,16 +1,11 @@
-local ruler = {
-    provider = " %4l/%L:%-3c %3p%%",
-}
+local ruler = { provider = " %4l/%L:%-3c %3p%%" }
 
 return {
     {
         hl = { fg = "fg", bg = "bg" },
-        astronvim.status.component.mode(),
+        astronvim.status.component.mode { mode_text = false },
         astronvim.status.component.git_branch(),
-        astronvim.status.component.file_info(
-        -- astronvim.is_available "bufferline.nvim" and { filetype = {}, filename = false, file_modified = false } or
-        -- nil
-        ),
+        astronvim.status.component.file_info(),
         astronvim.status.component.git_diff(),
         astronvim.status.component.diagnostics(),
         astronvim.status.component.fill(),
@@ -20,18 +15,5 @@ return {
         astronvim.status.component.treesitter(),
         ruler,
         astronvim.status.component.mode { surround = { separator = "right" } },
-    },
-    {
-        fallthrough = false,
-        {
-            condition = astronvim.status.condition.is_active,
-            astronvim.status.component.breadcrumbs { hl = { fg = "winbar_fg", bg = "winbar_bg" } },
-        },
-        astronvim.status.component.file_info {
-            unique_path = {},
-            file_icon = { hl = false },
-            hl = { fg = "winbarnc_fg", bg = "winbarnc_bg" },
-            surround = false,
-        },
     },
 }

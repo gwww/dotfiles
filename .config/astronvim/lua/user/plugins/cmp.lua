932fdd6a5f43331f1edaf3fa7583887860f8ac13
Glenn Waters <glenn@watrs.ca>
2022-12-05 17:01:41 -0500

    Mod cmp setup.

---
+++ .config/astronvim/lua/user/plugins/cmp.lua
@@ -0,0 +1,54 @@
+local vscode_kind_icons = {
+  Text = '  ',
+  Method = '  ',
+  Function = '  ',
+  Constructor = '  ',
+  Field = '  ',
+  Variable = '  ',
+  Class = '  ',
+  Interface = '  ',
+  Module = '  ',
+  Property = '  ',
+  Unit = '  ',
+  Value = '  ',
+  Enum = '  ',
+  Keyword = '  ',
+  Snippet = '  ',
+  Color = '  ',
+  File = '  ',
+  Reference = '  ',
+  Folder = '  ',
+  EnumMember = '  ',
+  Constant = '  ',
+  Struct = '  ',
+  Event = '  ',
+  Operator = '  ',
+  TypeParameter = '  ',
+}
+
+return {
+  -- completion = { completeopt = "menu,menuone,noinsert" },
+  window = {
+    documentation = { max_width = 40 },
+  },
+  formatting = {
+    fields = { "abbr", "kind", "menu" },
+    format = function(entry, vim_item)
+      -- Kind icons
+      vim_item.kind = string.format(' %s%-10s', vscode_kind_icons[vim_item.kind], vim_item.kind)
+      -- Source
+      vim_item.menu = ({
+        buffer = "[Buffer]",
+        nvim_lsp = "[LSP]",
+        luasnip = "[LuaSnip]",
+        nvim_lua = "[Lua]",
+        latex_symbols = "[LaTeX]",
+      })[entry.source.name]
+      return vim_item
+    end,
+    xformat = function(_, vim_item)
+      vim_item.kind = vscode_kind_icons[vim_item.kind] or ""
+      return vim_item
+    end,
+  },
+}

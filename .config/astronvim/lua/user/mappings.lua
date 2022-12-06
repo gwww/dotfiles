25a6f0eca25369f6af12e75709f09dd9befbc124
Glenn Waters <glenn@watrs.ca>
2022-12-05 22:47:52 -0500

    Split out bits of config

---
+++ .config/astronvim/lua/user/mappings.lua
@@ -0,0 +1,17 @@
+return {
+  n = {
+    [";"] = { ":", silent = false, desc = "Quick cmd" },
+    ["<enter>"] = { ":noh<enter><cr>", silent = true, desc = "No highlight" },
+    j = { "v:count==0 ? 'gj' : 'j'", expr = true },
+    k = { "v:count==0 ? 'gk' : 'k'", expr = true },
+    ["<leader>e"] = { "<cmd>Telescope find_files<cr>", desc = "Find files" },
+    ["<leader>E"] = { "<cmd>Neotree toggle<cr>", desc = "Toggle NeoTree" },
+    ["<leader>y"] = { '"*y', desc = "Yank to clipboard" },
+
+    ["<leader>ff"] = false,
+    ["<leader>h"] = false,
+  },
+  v = {
+    ["<leader>y"] = { '"*y', desc = "Yank to clipboard" },
+  },
+}
---
+++ .config/astronvim/lua/user/options.lua
@@ -0,0 +1,19 @@
+return {
+  opt = {
+    completeopt = "menuone,noinsert,noselect",
+    wildmode = "longest:full,full",
+    wildignorecase = true,
+
+    clipboard = "", -- Don't save to system clipboard; use keymap
+    confirm = true, -- Confirm quit on errors
+    cmdheight = 1, -- Override cmdheight=0
+    linebreak = true, -- Break at "natural" spot on wrap
+    relativenumber = false, -- Override
+    wrap = true, -- Wrap lines
+    whichwrap = "b,s,<,>,h,l,[,]",
+  },
+  g = {
+    mapleader = ",",
+    maplocalleader = ",",
+  },
+}

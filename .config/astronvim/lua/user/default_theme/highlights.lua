8bce9f9b155b9b9ebd5b1a65dbac7ba948bee7d0
Glenn Waters <glenn@watrs.ca>
2022-12-07 14:54:47 -0500

    Tweak colours.

--- .config/astronvim/lua/user/default_theme/highlights.lua
+++ .config/astronvim/lua/user/default_theme/highlights.lua
@@ -1,9 +1,14 @@
 return function(hl)
   hl.CursorLine.bg = "#151d28"
-  hl.Normal.bg = "#0e131b"
   hl.Comment.italic = true
   hl.IndentBlanklineChar.fg = "#282828"
   hl.IndentBlanklineContextChar.fg = "#3f3f3f"
+
+  hl.LspReferenceText.bg = "#202B3C"
+  hl.LspReferenceRead.bg = "#202B3C"
+  hl.LspReferenceWrite.bg = "#202B3C"
+
+  hl.IncSearch.bg = "#24450d"
 
   return hl
 end

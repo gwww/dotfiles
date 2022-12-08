711eaf44fa18d107fc0d4998bcf209aa12b5e7cf
Glenn Waters <glenn@watrs.ca>
2022-12-07 20:59:47 -0500

    Fix lsp config (should be in lsp directory, will do that later)

--- .config/astronvim/lua/user/init.lua
+++ .config/astronvim/lua/user/init.lua
@@ -6,4 +6,13 @@ return {
     underline = false,
     update_in_insert = false,
   },
+
+  lsp = {
+    on_attach = function(client, bufnr)
+      client.config.flags["debounce_text_changes"] = 500
+      if client.server_capabilities.documentHighlightProvider then
+        vim.api.nvim_del_augroup_by_name(vim.fn.printf("lsp_document_highlight_%d", bufnr))
+      end
+    end,
+  },
 }
--- .config/astronvim/lua/user/lsp.lua
+++
@@ -1,8 +0,0 @@
-return {
-  on_attach = function(client, bufnr)
-    client.config.flags["debounce_text_changes"] = 500
-    if client.server_capabilities.documentHighlightProvider then
-      vim.api.nvim_del_augroup_by_name(vim.fn.printf("lsp_document_highlight_%d", bufnr))
-    end
-  end,
-}

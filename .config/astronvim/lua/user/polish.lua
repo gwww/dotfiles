ece071ed07fe17a9cd07ca5df739cabb4b84bf96
Glenn Waters <glenn@watrs.ca>
2022-12-07 21:12:37 -0500

    Disable 2.9 auto hlsearch feature.

--- .config/astronvim/lua/user/polish.lua
+++ .config/astronvim/lua/user/polish.lua
@@ -1,4 +1,7 @@
 -- This function is run last and is a good place to configuring
 -- anything that doesn't fit in the normal config can go here
 
-return function() require "user.autocmds" end
+return function()
+  require "user.autocmds"
+  vim.on_key(nil, vim.api.nvim_get_namespaces()["auto_hlsearch"])
+end

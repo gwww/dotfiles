238ca78a39a5f04bdecde59bb26960e4a36fe56e
Glenn Waters <glenn@watrs.ca>
2022-12-11 17:51:30 -0500

    Get vim symbols for cmp.

--- .config/astronvim/lua/user/init.lua
+++ .config/astronvim/lua/user/init.lua
@@ -1,3 +1,13 @@
+local default_workspace = {
+  library = {
+    vim.fn.expand "$VIMRUNTIME",
+    require("neodev.config").types(),
+  },
+  checkThirdParty = false,
+  maxPreload = 5000,
+  preloadFileSize = 10000,
+}
+
 return {
   colorscheme = "nightfox",
 
@@ -9,10 +19,19 @@ return {
 
   lsp = {
     on_attach = function(client, bufnr)
-      client.config.flags["debounce_text_changes"] = 500
+      -- client.config.flags["debounce_text_changes"] = 500
       if client.server_capabilities.documentHighlightProvider then
         vim.api.nvim_del_augroup_by_name(vim.fn.printf("lsp_document_highlight_%d", bufnr))
       end
     end,
+    ["server-settings"] = {
+      ["sumneko_lua"] = {
+        settings = {
+          Lua = {
+            workspace = default_workspace,
+          },
+        },
+      },
+    },
   },
 }

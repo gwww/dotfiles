61025c40cb2b1ae5db6afb073f15cab89c614816
Glenn Waters <glenn@watrs.ca>
2022-12-11 20:23:18 -0500

    Tidy

--- .config/astronvim/lua/user/polish.lua
+++ .config/astronvim/lua/user/polish.lua
@@ -1,6 +1,5 @@
 -- This function is run last and is a good place to configuring
 -- anything that doesn't fit in the normal config can go here
-
 return function()
   require "user.autocmds"
   vim.on_key(nil, vim.api.nvim_get_namespaces()["auto_hlsearch"])

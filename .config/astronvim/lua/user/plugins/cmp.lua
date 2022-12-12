3962b72094dca25c7b27c7f04ccc0ca38b8ae430
Glenn Waters <glenn@watrs.ca>
2022-12-11 20:22:03 -0500

    Tidy cmp.

--- .config/astronvim/lua/user/plugins/cmp.lua
+++ .config/astronvim/lua/user/plugins/cmp.lua
@@ -27,21 +27,17 @@ local vscode_kind_icons = {
 }
 
 return {
-  -- completion = { completeopt = "menuone,noselect" },
   window = {
     documentation = { max_width = 40 },
   },
   formatting = {
     fields = { "abbr", "kind", "menu" },
     format = function(entry, vim_item)
-      -- Kind icons
-      vim_item.kind = string.format(" %s%-10s", vscode_kind_icons[vim_item.kind], vim_item.kind)
-      -- Source
+      vim_item.kind = string.format(" %s%s", vscode_kind_icons[vim_item.kind], vim_item.kind)
       vim_item.menu = ({
         buffer = "[Buffer]",
         nvim_lsp = "[LSP]",
-        luasnip = "[LuaSnip]",
-        nvim_lua = "[Lua]",
+        luasnip = "[Snip]",
         latex_symbols = "[LaTeX]",
       })[entry.source.name]
       return vim_item

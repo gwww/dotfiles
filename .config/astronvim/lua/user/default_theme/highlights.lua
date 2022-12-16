78924be4dcf5debbe453f99bc06457c71bb09f2a
Glenn Waters <glenn@watrs.ca>
2022-12-15 21:43:38 -0500

    Bufferline tweaks for new nightly functionality

--- .config/astronvim/lua/user/default_theme/highlights.lua
+++ .config/astronvim/lua/user/default_theme/highlights.lua
@@ -3,7 +3,7 @@ return function(hl)
   hl.CursorLine.bg = H.hslToHex(216, 31, 12)
   hl.Comment.italic = true
   hl.IndentBlanklineChar.fg = "#282828"
+  -- hl.IndentBlanklineChar.fg = "#173b14"
   hl.IndentBlanklineContextChar.fg = "#3f3f3f"
-  hl["CurSearch"] = { bg = "#a0e8af", fg = "#000000" }
   return hl
 end
--- .config/astronvim/lua/user/plugins/heirline.lua
+++ .config/astronvim/lua/user/plugins/heirline.lua
@@ -14,21 +14,5 @@ return function(config)
     { provider = " %4l/%L:%-3c %3p%%" },
     astronvim.status.component.mode { surround = { separator = "right" } },
   }
-
-  if vim.g.heirline_bufferline then -- TODO: remove if statement with v3 release
-    config[3][2] = astronvim.status.heirline.make_buflist(astronvim.status.component.file_info {
-      file_icon = { padding = { left = 1 } },
-      unique_path = { hl = { fg = "winbarnc_fg" } },
-      padding = { left = 1, right = 1 },
-      hl = function(self)
-        return {
-          fg = self.is_active and "tab_fg" or "tab_inactive_fg",
-          bold = self.is_active,
-        }
-      end,
-      surround = false,
-    })
-  end
-
   return config
 end

868d876100028ce3df12b67d74e0ddc505b71887
Glenn Waters <glenn@watrs.ca>
2023-01-22 23:48:37 -0500

    Add mappings.

--- .config/astronvim/lua/user/mappings.lua
+++ .config/astronvim/lua/user/mappings.lua
@@ -5,7 +5,10 @@ return {
     j = { "v:count==0 ? 'gj' : 'j'", expr = true },
     k = { "v:count==0 ? 'gk' : 'k'", expr = true },
     ["<leader>e"] = { "<cmd>Telescope find_files<cr>", desc = "Find files" },
+    ["<leader><leader>"] = { "<cmd>Telescope find_files<cr>", desc = "Find files" },
     ["<leader>E"] = { "<cmd>Neotree toggle<cr>", desc = "Toggle NeoTree" },
+    L = { "<cmd>bnext<cr>", desc = "Next buffer" },
+    H = { "<cmd>bprev<cr>", desc = "Prev buffer" },
     ["<leader>y"] = { '"*y', desc = "Yank to clipboard" },
   },
   v = {

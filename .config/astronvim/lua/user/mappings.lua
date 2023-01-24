43b37d9d8fce9183485ad8ab354e83c4643a3d0a
Glenn Waters <glenn@watrs.ca>
2023-01-23 19:27:58 -0500

    Add keymap. Change terminal colours.

--- .config/astronvim/lua/user/mappings.lua
+++ .config/astronvim/lua/user/mappings.lua
@@ -1,17 +1,29 @@
-return {
-  n = {
-    [";"] = { ":", silent = false, desc = "Quick cmd" },
-    ["<enter>"] = { ":noh<enter><cr>", silent = true, desc = "No highlight" },
-    j = { "v:count==0 ? 'gj' : 'j'", expr = true },
-    k = { "v:count==0 ? 'gk' : 'k'", expr = true },
-    ["<leader>e"] = { "<cmd>Telescope find_files<cr>", desc = "Find files" },
-    ["<leader><leader>"] = { "<cmd>Telescope find_files<cr>", desc = "Find files" },
-    ["<leader>E"] = { "<cmd>Neotree toggle<cr>", desc = "Toggle NeoTree" },
-    L = { "<cmd>bnext<cr>", desc = "Next buffer" },
-    H = { "<cmd>bprev<cr>", desc = "Prev buffer" },
-    ["<leader>y"] = { '"*y', desc = "Yank to clipboard" },
-  },
-  v = {
-    ["<leader>y"] = { '"*y', desc = "Yank to clipboard" },
-  },
-}
+local map = { n = {}, i = {}, v = {} }
+local input = map.i
+local normal = map.n
+local visual = map.v
+
+normal[';'] = { ":", silent = false, desc = "Quick cmd" }
+normal["<enter>"] = { ":noh<enter><cr>", silent = true, desc = "No highlight" }
+
+normal['j'] = { "v:count==0 ? 'gj' : 'j'", expr = true }
+normal['k'] = { "v:count==0 ? 'gk' : 'k'", expr = true }
+
+normal["<leader>e"] = { "<cmd>Telescope find_files<cr>", desc = "Find files" }
+normal["<leader><leader>"] = { "<cmd>Telescope find_files<cr>", desc = "Find files" }
+normal["<leader>E"] = { "<cmd>Neotree toggle<cr>", desc = "Toggle NeoTree" }
+
+normal['L'] = { "<cmd>bnext<cr>", desc = "Next buffer" }
+normal['H'] = { "<cmd>bprev<cr>", desc = "Prev buffer" }
+
+normal["<leader>y"] = { '"*y', desc = "Yank to clipboard" }
+visual["<leader>y"] = { '"*y', desc = "Yank to clipboard" }
+
+normal["<A-j>"] = { ":m .+1<cr>==", desc = "Move down" }
+visual["<A-j>"] = { ":m '>+1<cr>gv=gv", desc = "Move down" }
+input["<A-j>"] = { "<Esc>:m .+1<cr>==gi", desc = "Move down" }
+normal["<A-k>"] = { ":m .-2<cr>==", desc = "Move up" }
+visual["<A-k>"] = { ":m '<-2<cr>gv=gv", desc = "Move up" }
+input["<A-k>"] = { "<Esc>:m .-2<cr>==gi", desc = "Move up" }
+
+return map

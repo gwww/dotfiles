36240f882ae4d156856554991da2d0348580461c
Glenn Waters <glenn@watrs.ca>
2023-01-30 20:14:23 -0500

    Add treesitter default grammars.

--- .config/astronvim/lua/user/plugins/treesitter.lua
+++ .config/astronvim/lua/user/plugins/treesitter.lua
@@ -2,5 +2,15 @@ return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
     auto_install = true,
+    ensure_installed = {
+      "html",
+      "javascript",
+      "json",
+      "lua",
+      "python",
+      "svelte",
+      "typescript",
+      "yaml",
+    }
   },
 }

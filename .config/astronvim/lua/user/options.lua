41d6e5f169caa9bc9638534beda50e5cdda16129
Glenn Waters <glenn@watrs.ca>
2023-02-10 19:48:31 -0500

    Update for latest astronvim

--- .config/astronvim/lua/user/options.lua
+++ .config/astronvim/lua/user/options.lua
@@ -1,36 +1,36 @@
 return {
-  opt = {
-    -- completeopt = "menuone,noinsert,noselect",
-    wildmode = "longest:full,full",
-    wildignorecase = true,
+    opt = {
+        -- completeopt = "menuone,noinsert,noselect",
+        wildmode = "longest:full,full",
+        wildignorecase = true,
 
-    clipboard = "", -- Don't save to system clipboard; use keymap
-    confirm = true, -- Confirm quit on errors
-    cmdheight = 1, -- Override cmdheight=0
-    linebreak = true, -- Break at "natural" spot on wrap
-    relativenumber = false, -- Override
-    sessionoptions = { "buffers", "curdir", "tabpages", "winsize" },
-    timeoutlen = 300,
-    wrap = true, -- Wrap lines
-    whichwrap = "b,s,<,>,[,]", -- Motions that will wrap to prev/next line
-  },
-  g = {
-    mapleader = ",",
-    terminal_color_0 = "#000000",
-    terminal_color_1 = "#cc5555",
-    terminal_color_2 = "#55cc55",
-    terminal_color_3 = "#cdcd55",
-    terminal_color_4 = "#5455cb",
-    terminal_color_5 = "#cc55cc",
-    terminal_color_6 = "#7acaca",
-    terminal_color_7 = "#cccccc",
-    terminal_color_8 = "#555555",
-    terminal_color_9 = "#ff5555",
-    terminal_color_10 = "#55ff55",
-    terminal_color_11 = "#ffff55",
-    terminal_color_12 = "#5555ff",
-    terminal_color_13 = "#ff55ff",
-    terminal_color_14 = "#55ffff",
-    terminal_color_15 = "#ffffff",
-  },
+        clipboard = "", -- Don't save to system clipboard; use keymap
+        confirm = true, -- Confirm quit on errors
+        cmdheight = 1, -- Override cmdheight=0
+        linebreak = true, -- Break at "natural" spot on wrap
+        relativenumber = false, -- Override
+        sessionoptions = { "buffers", "curdir", "tabpages", "winsize" },
+        timeoutlen = 300,
+        wrap = true, -- Wrap lines
+        whichwrap = "b,s,<,>,[,]", -- Motions that will wrap to prev/next line
+    },
+    g = {
+        mapleader = ",",
+        terminal_color_0 = "#000000",
+        terminal_color_1 = "#cc5555",
+        terminal_color_2 = "#55cc55",
+        terminal_color_3 = "#cdcd55",
+        terminal_color_4 = "#5455cb",
+        terminal_color_5 = "#cc55cc",
+        terminal_color_6 = "#7acaca",
+        terminal_color_7 = "#cccccc",
+        terminal_color_8 = "#555555",
+        terminal_color_9 = "#ff5555",
+        terminal_color_10 = "#55ff55",
+        terminal_color_11 = "#ffff55",
+        terminal_color_12 = "#5555ff",
+        terminal_color_13 = "#ff55ff",
+        terminal_color_14 = "#55ffff",
+        terminal_color_15 = "#ffffff",
+    },
 }
--- .config/astronvim/lua/user/plugins/heirline.lua
+++ .config/astronvim/lua/user/plugins/heirline.lua
@@ -1,21 +1,22 @@
 return {
-  "rebelot/heirline.nvim",
-  opts = function(_, opts)
-    opts.statusline = {
-      hl = { fg = "fg", bg = "bg" },
-      astronvim.status.component.mode { mode_text = {} },
-      astronvim.status.component.git_branch(),
-      astronvim.status.component.file_info(),
-      astronvim.status.component.git_diff(),
-      astronvim.status.component.diagnostics(),
-      astronvim.status.component.fill(),
-      astronvim.status.component.cmd_info(),
-      astronvim.status.component.fill(),
-      astronvim.status.component.lsp(),
-      astronvim.status.component.treesitter(),
-      { provider = " %4l/%L:%-3c %3p%%" },
-      astronvim.status.component.mode { surround = { separator = "right" } },
-    }
-    return opts
-  end,
+    "rebelot/heirline.nvim",
+    opts = function(_, opts)
+      local status = require("core.utils.status")
+      opts.statusline = {
+          hl = { fg = "fg", bg = "bg" },
+          status.component.mode { mode_text = {} },
+          status.component.git_branch(),
+          status.component.file_info(),
+          status.component.git_diff(),
+          status.component.diagnostics(),
+          status.component.fill(),
+          status.component.cmd_info(),
+          status.component.fill(),
+          status.component.lsp(),
+          status.component.treesitter(),
+          { provider = " %4l/%L:%-3c %3p%%" },
+          status.component.mode { surround = { separator = "right" } },
+      }
+      return opts
+    end,
 }

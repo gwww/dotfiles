ac546f3dc7fe869519074d849d1d5abc8d5bf09d
Glenn Waters <glenn@watrs.ca>
2023-02-12 22:41:19 -0500

    Rename of lua LS

--- .config/astronvim/lua/user/plugins/mason-lspconfig.lua
+++ .config/astronvim/lua/user/plugins/mason-lspconfig.lua
@@ -1,18 +1,18 @@
 -- List of valid "ensure_installed" items.
 -- https://github.com/williamboman/mason-lspconfig.nvim/blob/main/lua/mason-lspconfig/mappings/server.lua
 return {
-  "williamboman/mason-lspconfig.nvim",
-  opts = {
-    automatic_installation = true,
-    ensure_installed = {
-      "html",
-      "jsonls",
-      "pyright",
-      "sumneko_lua",
-      "svelte",
-      "tailwindcss",
-      "tsserver",
-      "yamlls",
+    "williamboman/mason-lspconfig.nvim",
+    opts = {
+        automatic_installation = true,
+        ensure_installed = {
+            "html",
+            "jsonls",
+            "pyright",
+            "lua_ls",
+            "svelte",
+            "tailwindcss",
+            "tsserver",
+            "yamlls",
+        },
     },
-  },
 }

3bc3998b1b7d6bdc36558f7d4398a62419cb235d
Glenn Waters <glenn@watrs.ca>
2023-03-10 13:19:38 -0500

    Disable AstroNvim theme

--- .config/astronvim/lua/user/plugins/disabled.lua
+++ .config/astronvim/lua/user/plugins/disabled.lua
@@ -3,4 +3,5 @@ return {
   { "rcarriga/nvim-dap-ui",                enabled = false },
   { "goolord/alpha-nvim",                  enabled = false },
   { "lukas-reineke/indent-blankline.nvim", enabled = false },
+  { "AstroNvim/astrotheme",                enabled = false },
 }

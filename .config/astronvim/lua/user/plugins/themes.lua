aa5cf44db62d3886392ba5fe75b7cac6a9727506
Glenn Waters <glenn@watrs.ca>
2023-03-13 16:32:33 -0400

    Remove nightfox.

--- .config/astronvim/lua/user/plugins/themes.lua
+++ .config/astronvim/lua/user/plugins/themes.lua
@@ -25,30 +25,4 @@ return {
       },
     },
   },
-
-  {
-    "EdenEast/nightfox.nvim",
-    priority = 1000,
-    opts = {
-      options = {
-        styles = {
-          comments = "italic",
-        },
-      },
-      palettes = {
-        nightfox = {
-          bg1 = H.hslToHex(216, 31, 8),
-          bg3 = H.hslToHex(216, 31, 12),
-          comment = "#7687a0",
-        },
-      },
-      groups = {
-        nightfox = {
-          DiffText = { bg = "darkgreen" },
-          IndentBlanklineChar = { fg = "#282828" },
-          IndentBlanklineContextChar = { fg = "#3f3f3f" },
-        },
-      },
-    },
-  },
 }

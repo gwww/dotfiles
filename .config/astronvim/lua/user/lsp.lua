9cbe517807988dc1fc02a2fe05347257d42563d0
Glenn Waters <glenn@watrs.ca>
2023-06-12 11:49:27 -0400

    Multiple tweaks

--- .config/astronvim/lua/user/lsp.lua
+++ .config/astronvim/lua/user/lsp.lua
@@ -1,7 +1,7 @@
 return {
   on_attach = function(client)
     if client.server_capabilities.documentHighlightProvider then
-      -- Turn of highlight of symbol under cursor
+      -- Turn off highlight of symbol under cursor
       vim.api.nvim_del_augroup_by_name "lsp_document_highlight"
     end
   end,
--- .config/astronvim/lua/user/plugins/mason-lspconfig.lua
+++ .config/astronvim/lua/user/plugins/mason-lspconfig.lua
@@ -7,7 +7,7 @@ return {
       automatic_installation = true,
       ensure_installed = {
         "html",
-        "jsonls",
+        -- "jsonls",
         "pyright",
         "lua_ls",
         "svelte",

a35051413ab488549701739395b9be8a156c23ef
Glenn Waters <glenn@watrs.ca>
2024-02-07 18:57:58 -0500

    Change how mise is initialized.

--- .config/fish/conf.d/10-login.fish
+++ .config/fish/conf.d/10-login.fish
@@ -12,10 +12,11 @@ set -gx GNUPGHOME ~/.local/share/gnupg
 set -gx POETRY_HOME ~/.local/share/pypoetry
 set -gx RIPGREP_CONFIG_PATH ~/.config/ripgrep/ripgreprc
 set -gx PNPM_HOME /Users/glenn/Library/pnpm
+set -gx MISE_FISH_AUTO_ACTIVATE 0
 
 set -gx CUPS_CACHEDIR ~/.local/state/cups
 set -gx CUPS_STATEDIR ~/.local/state/cups
 set -gx CUPS_DATADIR ~/.local/state/cups
 
 set -l brewcmd (path filter /opt/homebrew/bin/brew /usr/local/bin/brew)[1]; and $brewcmd shellenv | source
-set -gx PATH ~/bin $POETRY_HOME/bin $PNPM_HOME $PATH
+set -gx PATH ~/bin $POETRY_HOME/bin ~/.local/share/mise/shims $PNPM_HOME $PATH

a34eede8b5d3953fde690315aa166091eb8ae3bc
Glenn Waters <glenn@watrs.ca>
2024-02-13 15:27:12 -0500

    Fix typo

--- .config/AstroV4/lua/plugins/luasnip.lua
+++ .config/AstroV4/lua/plugins/luasnip.lua
@@ -3,7 +3,7 @@ return {
   config = function(plugin, opts)
     require "astronvim.plugins.configs.luasnip"(plugin, opts)
     require("luasnip.loaders.from_vscode").lazy_load()
-    -- require("luasnip.loaders.from_vscode").lazy_load { paths = { vim.fn.stdpath "config" .. "snippets" } }
+    require("luasnip.loaders.from_vscode").lazy_load { paths = { vim.fn.stdpath "config" .. "/snippets" } }
   end,
   dependencies = { "rafamadriz/friendly-snippets" },
 }

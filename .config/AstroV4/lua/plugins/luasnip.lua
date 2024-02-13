33b6771a93796835d53f6af3c55cecc2906985e9
Glenn Waters <glenn@watrs.ca>
2024-02-12 19:55:19 -0500

    Add luasnip.

---
+++ .config/AstroV4/lua/plugins/luasnip.lua
@@ -0,0 +1,9 @@
+return {
+  "L3MON4D3/LuaSnip",
+  config = function(plugin, opts)
+    require "astronvim.plugins.configs.luasnip"(plugin, opts)
+    require("luasnip.loaders.from_vscode").lazy_load()
+    -- require("luasnip.loaders.from_vscode").lazy_load { paths = { vim.fn.stdpath "config" .. "snippets" } }
+  end,
+  dependencies = { "rafamadriz/friendly-snippets" },
+}

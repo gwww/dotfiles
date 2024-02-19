721bc9be7b182bf367ebe9b4390361286d17d897
Glenn Waters <glenn@watrs.ca>
2024-02-18 20:15:51 -0500

    Cleanup keymap; add map for window resize.

--- .config/AstroV4/lua/plugins/AstroCore.lua
+++ .config/AstroV4/lua/plugins/AstroCore.lua
@@ -2,28 +2,18 @@
 local keymap = { n = {}, i = {}, v = {} }
 local input, normal, visual = keymap.i, keymap.n, keymap.v
 
-normal[";"] = { ":", silent = false, desc = "Quick cmd" }
+normal[";"] = { ":", silent = false, desc = "Enter command mode" }
 normal["<Leader>e"] = { "<cmd>Telescope find_files<cr>", desc = "Find files" }
--- normal["<space>"] = { "<cmd>Telescope buffers<cr>", desc = "Find buffers" }
 normal["<space>"] = {
   function()
     if #vim.t.bufs > 1 then vim.api.nvim_command "Telescope buffers" end
   end,
   desc = "Find buffers",
 }
-normal["L"] = {
-  function() require("astrocore.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
-  desc = "Next buffer",
-}
-normal["H"] = {
-  function() require("astrocore.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
-  desc = "Previous buffer",
-}
 normal["<Leader>y"] = { '"*y', desc = "Yank to clipboard" }
 visual["<Leader>y"] = { '"*y', desc = "Yank to clipboard" }
-
 normal["<enter>"] = { "<cmd>noh<Enter><cr>", silent = true, desc = "No highlight" }
-normal["<esc>"] = { "<cmd>noh<cr>", silent = true, desc = "No highlight" }
+normal["<Leader>w"] = { function() require("smart-splits").start_resize_mode() end, desc = "Enter window resize mode" }
 
 normal["<A-j>"] = { ":m .+1<cr>==", desc = "Move down" }
 visual["<A-j>"] = { ":m '>+1<cr>gv=gv", desc = "Move down" }
@@ -57,13 +47,12 @@ return {
         linebreak = true, -- Break at "natural" spot on wrap
         relativenumber = false, -- Override
         sessionoptions = { "buffers", "curdir", "tabpages", "winsize" },
+        showbreak = "↪ ",
         showtabline = 0, -- Disable the tabline with the buffer list
         timeoutlen = 300,
         title = false,
         wrap = true, -- Wrap lines
         whichwrap = "b,s,<,>,[,]", -- Motions that will wrap to prev/next line
-
-        showbreak = "↪ ",
       },
     },
   },

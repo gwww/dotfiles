189c8b27a8a07f84e88ecf673770cdfc234eb8b7
Glenn Waters <glenn@watrs.ca>
2024-03-02 20:41:07 -0500

    Add endwise.

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,8 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  dependencies = { "RRethy/nvim-treesitter-endwise" },
   opts = function(_, opts)
+    opts.endwise = { enable = true }
     opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
       "bash",
       "c",

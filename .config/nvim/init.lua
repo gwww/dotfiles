79001001e5b3976019c1fdd4a946caec1506baa7
Glenn Waters <glenn@watrs.ca>
2024-03-07 21:27:54 -0500

    Tidy

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,4 @@
 -- bootstrap lazy.nvim, AstroNvim, and user plugins
--- You should not have to touch this file
 
 local lazypath = vim.env.LAZY or vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 if not (vim.env.LAZY or vim.loop.fs_stat(lazypath)) then
@@ -8,7 +7,7 @@ if not (vim.env.LAZY or vim.loop.fs_stat(lazypath)) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local lazy_loaded, lazy = pcall(require, "lazy") -- validate that lazy is available
+local lazy_loaded, lazy = pcall(require, "lazy")
 if not lazy_loaded then
   -- stylua: ignore
   vim.api.nvim_echo({ { ("Unable to load lazy from: %s\n"):format(lazypath), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } }, true, {})
@@ -20,11 +19,9 @@ pcall(require, "pre-setup")
 
 lazy.setup({
   { "AstroNvim/AstroNvim", branch = "v4", import = "astronvim.plugins" }, -- TODO: change `branch="v4"` to `version="^4"` on release
-
   { import = "plugins" }, -- import my plugins
 }, {
-  install = { colorscheme = { "catppuccin", "astrodark", "habamax" } },
-  -- lockfile = vim.fn.stdpath "data" .. "/lazy-lock.json",
+  install = { colorscheme = { "nord", "catppuccin", "astrodark", "habamax" } },
   ui = { border = "rounded" },
   performance = {
     rtp = {

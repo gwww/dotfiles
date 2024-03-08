16fc719f2bcf742993dc32a3e231a6148c10bb28
Glenn Waters <glenn@watrs.ca>
2024-03-07 21:02:26 -0500

    Tweaks

--- .config/wezterm/keys.lua
+++ .config/wezterm/keys.lua
@@ -1,20 +1,19 @@
-local wt = require("wezterm")
+local wt = require "wezterm"
 local keys = {}
 
-local mapkey =
-    function(mods, key, action) keys[#keys + 1] = { key = key, mods = mods, action = action } end
+local mapkey = function(mods, key, action) keys[#keys + 1] = { key = key, mods = mods, action = action } end
 
 local edit_wezterm_config = function()
-    local files = "wezterm.lua keys.lua events.lua"
-    local args = { os.getenv("SHELL"), "-lic", "exec $VISUAL " .. files }
-    return wt.action.SpawnCommandInNewTab {cwd=wt.config_dir, args=args}
+  local files = "config.lua keys.lua events.lua"
+  local args = { os.getenv "SHELL", "-lic", "exec $VISUAL " .. files }
+  return wt.action.SpawnCommandInNewTab { cwd = wt.config_dir, args = args }
 end
 
 mapkey("CMD", "d", wt.action.SplitHorizontal)
 mapkey("CMD|SHIFT", "d", wt.action.SplitVertical)
 mapkey("CMD", "e", edit_wezterm_config())
 mapkey("CTRL|SHIFT", "l", "ShowDebugOverlay")
-mapkey("CMD", "w", wt.action.CloseCurrentPane {confirm = true})
+mapkey("CMD", "w", wt.action.CloseCurrentPane { confirm = true })
 mapkey("CMD", "z", "TogglePaneZoomState")
 
 return keys
--- .local/README.md
+++
@@ -1,8 +0,0 @@
-# .local directory
-The purpose of this directory is to hold all the files that are either
-generated (e.g.: shell history, vim editing info, ...) or cloned (e.g.: 
-vim plugins, ...).
-
-The files here are temporal or easily recreatable.
-
-Other than this README no files in this directory are under version control.

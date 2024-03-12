a6ec44b0af2c6d92047052336beb811fdc0572f4
Glenn Waters <glenn@watrs.ca>
2024-03-11 21:12:36 -0400

    Cleanup wezterm config

--- .config/wezterm/events.lua
+++ .config/wezterm/events.lua
@@ -1,16 +1,14 @@
-local wt = require("wezterm")
+local wt = require "wezterm"
 
 wt.on(
     "window-config-reloaded",
-    function(window, pane) window:toast_notification("Configuration loaded.", "", nil, 5000) end
+    function(window, _) window:toast_notification("Configuration loaded.", "", nil, 5000) end
 )
 
 -- This produces a window split horizontally into two equal parts
 wt.on("gui-startup", function(cmd)
-    local tab, pane, window = wt.mux.spawn_window(cmd or {})
-    if wt.gui.screens().active.name == "Built-in Retina Display" then
-        window:gui_window():maximize()
-    end
+    local _, _, window = wt.mux.spawn_window(cmd or {})
+    if wt.gui.screens().active.name == "Built-in Retina Display" then window:gui_window():maximize() end
     -- pane:split {size=0.3}
     -- pane:split {direction="Left", size=0.5}
 end)

ef017cb375501f3b3de01a1b5f000d165e72a873
Glenn Waters <glenn@watrs.ca>
2024-03-14 20:18:19 -0400

    Switch color scheme.

--- .config/nvim/lua/plugins/AstroUI.lua
+++ .config/nvim/lua/plugins/AstroUI.lua
@@ -1,29 +1,25 @@
 -- Config Docs: https://github.com/AstroNvim/astroui#%EF%B8%8F-configuration
-local base, mantle = "#1e2129", "#23262f"
-local color_mods = { Normal = { bg = base }, SignColumn = { bg = base }, CursorLine = { bg = "#23262f" } }
-
 return {
   {
     "AstroNvim/astroui",
     opts = {
-      colorscheme = "catppuccin-mocha",
+      colorscheme = "nordfox",
       highlights = {
-        nord = color_mods,
-        onenord = color_mods,
+        nordfox = { CursorLine = { bg = "#23262f" } },
       },
       status = { separators = { left = { "", " " }, right = { " ", "" } } },
     },
   },
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
+    "EdenEast/nightfox.nvim",
     lazy = true,
     priority = 1000,
     opts = {
-      dim_inactive = { enabled = true, shade = "light", percentage = 0.25 },
-      color_overrides = { mocha = { base = base, mantle = mantle, crust = "#474747" } },
+      palettes = {
+        nordfox = {
+          bg1 = "#1e2129",
+        },
+      },
     },
   },
-  { "gbprod/nord.nvim", lazy = true, priority = 1000 },
-  { "rmehri01/onenord.nvim", lazy = true, priority = 1000 },
 }

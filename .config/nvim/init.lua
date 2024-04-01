5dc3b628ff54203986aa3adb7157b36001b731b2
Glenn Waters <glenn@watrs.ca>
2024-04-01 11:10:51 -0400

    Upgrade for AstroNvim v4

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,14 +3,17 @@
 local lazypath = vim.env.LAZY or vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 if not (vim.env.LAZY or vim.loop.fs_stat(lazypath)) then
   -- stylua: ignore
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable",
+    lazypath })
 end
 vim.opt.rtp:prepend(lazypath)
 
 local lazy_loaded, lazy = pcall(require, "lazy")
 if not lazy_loaded then
   -- stylua: ignore
-  vim.api.nvim_echo({ { ("Unable to load lazy from: %s\n"):format(lazypath), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } }, true, {})
+  vim.api.nvim_echo(
+    { { ("Unable to load lazy from: %s\n"):format(lazypath), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } },
+    true, {})
   vim.fn.getchar()
   vim.cmd.quit()
 end
@@ -18,7 +21,7 @@ end
 pcall(require, "pre-setup")
 
 lazy.setup({
-  { "AstroNvim/AstroNvim", branch = "v4", import = "astronvim.plugins" }, -- TODO: change `branch="v4"` to `version="^4"` on release
+  { "AstroNvim/AstroNvim", version = "^4", import = "astronvim.plugins" },
   { import = "plugins" }, -- import my plugins
 }, {
   install = { colorscheme = { "nord", "catppuccin", "astrodark", "habamax" } },

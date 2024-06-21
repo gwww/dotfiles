6faa61d8b427a6cc844e3577db39c69e60323b70
Glenn Waters <glenn@watrs.ca>
2024-06-21 14:13:04 -0400

    Add conform
    Added to use ruff import sorting.

--- .config/nvim/lua/plugins/AstroLSP.lua
+++ .config/nvim/lua/plugins/AstroLSP.lua
@@ -39,16 +39,13 @@ return {
     end,
   },
 
-  -- {
-  --   "jay-babu/mason-nvim-dap.nvim",
-  --   -- overrides `require("mason-nvim-dap").setup(...)`
-  --   opts = function(_, opts)
-  --     -- add more things to the ensure_installed table protecting against community packs modifying it
-  --     opts.ensure_installed = require("astrocore").list_insert_unique(
-  --       opts.ensure_installed,
-  --       "python"
-  --       -- add more arguments for adding more debuggers
-  --     )
-  --   end,
-  -- },
+  {
+    "stevearc/conform.nvim",
+    -- optional = true,
+    opts = {
+      formatters_by_ft = {
+        python = { "ruff_organize_imports", "ruff_format" },
+      },
+    },
+  },
 }

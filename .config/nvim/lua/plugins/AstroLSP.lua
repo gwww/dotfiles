2db6bcab11b86ea1876ba2e218fd2739eafe00b4
Glenn Waters <glenn@watrs.ca>
2024-06-20 20:48:23 -0400

    Add ruff; remove isort, black.

--- .config/nvim/lua/plugins/AstroLSP.lua
+++ .config/nvim/lua/plugins/AstroLSP.lua
@@ -1,19 +1,54 @@
--- Config Docs: https://github.com/AstroNvim/astrolsp#%EF%B8%8F-configuration
-
 return {
-  "AstroNvim/astrolsp",
-  opts = {
-    -- formatting = {
-    --   disabled = {
-    --     "lua_ls",
-    --   },
-    -- },
-
-    -- on_attach = function(client, _)
-    --   if client.server_capabilities.documentHighlightProvider then
-    --     -- Turn off highlight of symbol under cursor
-    --     vim.api.nvim_del_augroup_by_name "lsp_document_highlight"
-    --   end
-    -- end,
+  {
+    -- Config Docs: https://github.com/AstroNvim/astrolsp#%EF%B8%8F-configuration
+    "AstroNvim/astrolsp",
+    opts = {
+      config = {
+        ruff = { on_attach = function(client) client.server_capabilities.hoverProvider = false end },
+      },
+    },
   },
+
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+        "html",
+        "pyright",
+        "lua_ls",
+        "ruff",
+        "svelte",
+        "tailwindcss",
+        "tsserver",
+        "yamlls",
+      })
+    end,
+  },
+
+  {
+    "jay-babu/mason-null-ls.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+        "prettier",
+        "stylua",
+      })
+      opts.ensure_installed = vim.tbl_filter(
+        function(v) return not vim.tbl_contains({ "black", "isort" }, v) end,
+        opts.ensure_installed
+      )
+    end,
+  },
+
+  -- {
+  --   "jay-babu/mason-nvim-dap.nvim",
+  --   -- overrides `require("mason-nvim-dap").setup(...)`
+  --   opts = function(_, opts)
+  --     -- add more things to the ensure_installed table protecting against community packs modifying it
+  --     opts.ensure_installed = require("astrocore").list_insert_unique(
+  --       opts.ensure_installed,
+  --       "python"
+  --       -- add more arguments for adding more debuggers
+  --     )
+  --   end,
+  -- },
 }

ad4682d225e4605a16fa3871bdc1f7dd248a8420
Glenn Waters <glenn@watrs.ca>
2024-07-04 21:00:27 -0400

    Multiple changes.
    Add shell integration to prompt.
    Add kitty to hammerspoon launch.
    Bump lazy lock file.
    Update brewfile.

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -2,13 +2,15 @@ function fish_prompt --description 'Write out the prompt'
   test $status -ne 0; and set -l c (set_color red); or set -l c (set_color green)
   fish_is_root_user; and set -l prompt_char $c'#'; or set -l prompt_char $c'λ'
 
+  echo -en "\e]133;A\007"
+
   echo -n (set_color 7df9aa)(date +%H:%M:%S)' '
   test -n "$SSH_CLIENT" -o -n "$SSH_TTY"; and echo -n (hostname -s)' '
   echo -n (set_color 00ffff) (prompt_pwd -D 3)' '
 
   # Python VENV
   test -n "$VIRTUAL_ENV"; and echo -n (set_color 00d7ff) (string split -r -m 2 -f 2 '/' $VIRTUAL_ENV)' '
-   
+
   set -l git_status (git status --branch --porcelain=2 2> /dev/null)
   if test -n "$git_status"
     set -l branch (string match -r '^# branch.head (.*)' $git_status)[2]
@@ -18,7 +20,8 @@ function fish_prompt --description 'Write out the prompt'
       set branch (string match -r '^# branch.oid (.......)' $git_status)[2]
     else
       set updown (string match -r '^# branch.ab [+-]?([\d]+) [+-]?([\d]+)' $git_status)
-      if [ "$updown[2] $updown[3]" != "0 0" ]
+      # if [ "$updown[2] $updown[3]" != "0 0" ]
+      if test $status -eq 0 -a "$updown[2] $updown[3]" != "0 0"
         echo -n (set_color bryellow) $updown[2]$updown[3]' '
       end
       test -z "$(string match -r '^[^#]' $git_status)"; and set c "green"; or set c "red"
@@ -27,4 +30,14 @@ function fish_prompt --description 'Write out the prompt'
   end
 
   echo -n $prompt_char (set_color normal)
+
+  echo -en "\e]133;B\007"
+end
+
+function _mark_output_start --on-event fish_preexec
+  printf '\e]133;C;cmdline_url=%s\007' (string escape -- "$argv")
+end
+
+function _mark_output_end --on-event fish_postexec
+  echo -en "\e]133;D;$status\007"
 end
--- .config/hammerspoon/init.lua
+++ .config/hammerspoon/init.lua
@@ -14,66 +14,70 @@ hyper.init "F20"
 hyper.addHook(nil, function(_) end) -- ignore hyper key press/realease
 
 hyper.bind {
-    -- Table of app bindings. Each binding can have a "key", "modifiers", & "action".
-    {
-        key = "a",
-        action = function() sl.smartLaunch("com.binarynights.ForkLift", "ForkLift") end,
-    },
-    { key = "c", action = caffeinate },
-    {
-        key = "f",
-        action = function() sl.smartLaunch "Finder" end,
-    },
-    {
-        key = "h",
-        action = function() os.execute "open ~" end,
-    },
-    {
-        key = "q",
-        action = function() sl.smartLaunch "Firefox" end,
-    },
-    { key = "r", action = hs.reload },
-    {
-        key = "v",
-        action = function() hs.eventtap.keyStrokes(hs.pasteboard.getContents()) end,
-    },
-    {
-        key = "w",
-        action = function() sl.smartLaunch "WezTerm" end,
-    },
-    { key = "y",     action = hs.toggleConsole },
-    {
-        key = "z",
-        action = function() sl.smartLaunch "zoom.us" end,
-    },
+  -- Table of app bindings. Each binding can have a "key", "modifiers", & "action".
+  {
+    key = "a",
+    action = function() sl.smartLaunch("com.binarynights.ForkLift", "ForkLift") end,
+  },
+  { key = "c", action = caffeinate },
+  {
+    key = "f",
+    action = function() sl.smartLaunch "Finder" end,
+  },
+  {
+    key = "h",
+    action = function() os.execute "open ~" end,
+  },
+  {
+    key = "q",
+    action = function() sl.smartLaunch "Firefox" end,
+  },
+  { key = "r", action = hs.reload },
+  {
+    key = "v",
+    action = function() hs.eventtap.keyStrokes(hs.pasteboard.getContents()) end,
+  },
+  {
+    key = "e",
+    action = function() sl.smartLaunch "WezTerm" end,
+  },
+  {
+    key = "w",
+    action = function() sl.smartLaunch("net.kovidgoyal.kitty", "kitty") end,
+  },
+  { key = "y",     action = hs.toggleConsole },
+  {
+    key = "z",
+    action = function() sl.smartLaunch "zoom.us" end,
+  },
 
-    {
-        key = "left",
-        action = wf.cycleCalls(wf.toGrid, { { 0, 0, 0.5, 1 }, { 0, 0, 2 / 3, 1 }, { 0, 0, 1 / 3, 1 } }),
-    },
-    {
-        key = "right",
-        action = wf.cycleCalls(wf.toGrid, { { 0.5, 0, 0.5, 1 }, { 1 / 3, 0, 2 / 3, 1 }, { 2 / 3, 0, 1 / 3, 1 } }),
-    },
-    {
-        key = "left",
-        modifiers = "shift",
-        action = function() wf.moveScreen "West" end,
-    },
-    {
-        key = "right",
-        modifiers = "shift",
-        action = function() wf.moveScreen "East" end,
-    },
-    {
-        key = "up",
-        action = function() wf.toGrid { 0, 0, 1, 0.3 } end,
-    },
-    {
-        key = "down",
-        action = function() wf.toGrid { 0, 0.7, 1, 0.3 } end,
-    },
-    { key = "space", action = wf.toggleMaximize },
+  {
+    key = "left",
+    action = wf.cycleCalls(wf.toGrid, { { 0, 0, 0.5, 1 }, { 0, 0, 2 / 3, 1 }, { 0, 0, 1 / 3, 1 } }),
+  },
+  {
+    key = "right",
+    action = wf.cycleCalls(wf.toGrid, { { 0.5, 0, 0.5, 1 }, { 1 / 3, 0, 2 / 3, 1 }, { 2 / 3, 0, 1 / 3, 1 } }),
+  },
+  {
+    key = "left",
+    modifiers = "shift",
+    action = function() wf.moveScreen "West" end,
+  },
+  {
+    key = "right",
+    modifiers = "shift",
+    action = function() wf.moveScreen "East" end,
+  },
+  {
+    key = "up",
+    action = function() wf.toGrid { 0, 0, 1, 0.3 } end,
+  },
+  {
+    key = "down",
+    action = function() wf.toGrid { 0, 0.7, 1, 0.3 } end,
+  },
+  { key = "space", action = wf.toggleMaximize },
 }
 
 -- smartLaunch needs to hook into hyper

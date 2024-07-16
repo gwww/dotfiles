0996896ff586be16133500fb129a8bb80c5e436a
Glenn Waters <glenn@watrs.ca>
2024-07-16 10:44:19 -0400

    Added astrocommunity

---
+++ .config/nvim/lua/astrocommunity.lua
@@ -0,0 +1,7 @@
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.file-explorer.oil-nvim" },
+  -- { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.python-ruff" },
+  { import = "astrocommunity.syntax.vim-sandwich" },
+}

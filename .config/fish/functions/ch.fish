bcfe1684fdb181e96412e7ce846377439b7c8a60
Glenn Waters <glenn@watrs.ca>
2024-08-26 12:10:40 -0400

    Misc updates

--- .config/fish/functions/ch.fish
+++ .config/fish/functions/ch.fish
@@ -8,22 +8,25 @@ end
 
 function ch --description 'Display the changelog for the named argument'
   if test (count $argv) -ge 1
-    if string match -- "$argv[1]" "fd" >/dev/null
-      _md_file https://raw.githubusercontent.com/sharkdp/fd/master/CHANGELOG.md
-    else if string match -- "$argv[1]" "bat" >/dev/null
+    if string match -- "$argv[1]" "bat" >/dev/null
       _md_file https://raw.githubusercontent.com/sharkdp/bat/master/CHANGELOG.md
-    else if string match -- "$argv[1]" "poetry" >/dev/null
-      _md_file https://raw.githubusercontent.com/python-poetry/poetry/master/CHANGELOG.md
-    else if string match -- "$argv[1]" "mise" >/dev/null
-      _md_file https://raw.githubusercontent.com/jdx/mise/main/CHANGELOG.md
-    else if string match -- "$argv[1]" "fzf" >/dev/null
-      _md_file https://raw.githubusercontent.com/junegunn/fzf/master/CHANGELOG.md
-    else if string match -- "$argv[1]" "ripgrep" >/dev/null
-      _md_file https://raw.githubusercontent.com/BurntSushi/ripgrep/master/CHANGELOG.md
     else if string match -- "$argv[1]" "eza" >/dev/null
       _md_file https://raw.githubusercontent.com/eza-community/eza/main/CHANGELOG.md
+    else if string match -- "$argv[1]" "fd" >/dev/null
+      _md_file https://raw.githubusercontent.com/sharkdp/fd/master/CHANGELOG.md
+    else if string match -- "$argv[1]" "fzf" >/dev/null
+      _md_file https://raw.githubusercontent.com/junegunn/fzf/master/CHANGELOG.md
+    else if string match -- "$argv[1]" "mise" >/dev/null
+      _md_file https://raw.githubusercontent.com/jdx/mise/main/CHANGELOG.md
+    else if string match -- "$argv[1]" "poetry" >/dev/null
+      _md_file https://raw.githubusercontent.com/python-poetry/poetry/master/CHANGELOG.md
+    else if string match -- "$argv[1]" "ripgrep" >/dev/null
+      _md_file https://raw.githubusercontent.com/BurntSushi/ripgrep/master/CHANGELOG.md
+    else if string match -- "$argv[1]" "ruff" >/dev/null
+      _md_file https://raw.githubusercontent.com/astral-sh/ruff/main/CHANGELOG.md
     else if string match -- "$argv[1]" "zoxide" >/dev/null
       _md_file https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/CHANGELOG.md
+
     else if string match -- "$argv[1]" "lazygit" >/dev/null
       _releases_page https://github.com/jesseduffield/lazygit/releases
     else if string match -- "$argv[1]" "neovim" >/dev/null

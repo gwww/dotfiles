eef62cc4ed350ced77482e8cde404936ea8b2517
Glenn Waters <glenn@watrs.ca>
2024-08-28 10:20:52 -0400

    Remove commented code; update for deprecated

--- .config/nvim/lua/pre-setup.lua
+++ .config/nvim/lua/pre-setup.lua
@@ -1,9 +1,2 @@
 -- mapleader must be set before Lazy setup is called, just because
 vim.g.mapleader = ","
-
--- HACK: This shifty 1ms delay prevents a libuv error and nvim exiting with code 134 on `:wq`.
--- vim.api.nvim_create_autocmd({ "VimLeave" }, {
---   callback = function()
---     vim.cmd("sleep 1m")
---   end,
--- })

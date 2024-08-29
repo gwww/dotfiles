eeda8732b9b2bbce87a90b02cf3c9da915de08ca
Glenn Waters <glenn@watrs.ca>
2024-08-29 11:40:40 -0400

    Tidy

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,17 +3,14 @@
 local lazypath = vim.env.LAZY or vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 if not (vim.env.LAZY or vim.uv.fs_stat(lazypath)) then
   -- stylua: ignore
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable",
-    lazypath })
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
 vim.opt.rtp:prepend(lazypath)
 
 local lazy_loaded, lazy = pcall(require, "lazy")
 if not lazy_loaded then
   -- stylua: ignore
-  vim.api.nvim_echo(
-    { { ("Unable to load lazy from: %s\n"):format(lazypath), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } },
-    true, {})
+  vim.api.nvim_echo({ { ("Unable to load lazy from: %s\n"):format(lazypath), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } }, true, {})
   vim.fn.getchar()
   vim.cmd.quit()
 end

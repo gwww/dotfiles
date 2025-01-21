8506d54525e52c21164c885c6953ce8b3a9f2325
Glenn Waters <glenn@watrs.ca>
2025-01-21 14:52:26 -0500

    Formatting.

--- .config/nvim/lua/plugins/snacks-picker.lua
+++ .config/nvim/lua/plugins/snacks-picker.lua
@@ -7,79 +7,60 @@ return {
   specs = {
     {
       "AstroNvim/astroui",
-      opts = {
-        highlights = { init = { SnacksPickerDir = { fg = "#626875" } } },
-      },
+      opts = { highlights = { init = { SnacksPickerDir = { fg = "#626875" } } } },
     },
     {
       "AstroNvim/astrocore",
       opts = function(_, opts)
         local maps = opts.mappings
+        local p = require("snacks").picker
         maps.n["<Leader>f"] = vim.tbl_get(opts, "_map_sections", "f")
         if vim.fn.executable "git" == 1 then
           maps.n["<Leader>g"] = vim.tbl_get(opts, "_map_sections", "g")
-          maps.n["<Leader>gb"] = { function() require("snacks").picker.git_branches() end, desc = "Git branches" }
-          maps.n["<Leader>gc"] = {
-            function() require("snacks").picker.git_log() end,
-            desc = "Git commits (repository)",
-          }
-          maps.n["<Leader>gC"] = {
-            function() require("snacks").picker.git_log { current_file = true, follow = true } end,
-            desc = "Git commits (current file)",
-          }
-          maps.n["<Leader>gt"] = { function() require("snacks").picker.git_status() end, desc = "Git status" }
+          maps.n["<Leader>gb"] = { function() p.git_branches() end, desc = "Git branches" }
+          maps.n["<Leader>gc"] = { function() p.git_log() end, desc = "Git commits (repository)" }
+          maps.n["<Leader>gC"] =
+            { function() p.git_log { current_file = true, follow = true } end, desc = "Git commits (current file)" }
+          maps.n["<Leader>gt"] = { function() p.git_status() end, desc = "Git status" }
         end
-        maps.n["<Leader>f<CR>"] = { function() require("snacks").picker.resume() end, desc = "Resume previous search" }
-        maps.n["<Leader>f'"] = { function() require("snacks").picker.marks() end, desc = "Find marks" }
-        maps.n["<Leader>fl"] = {
-          function() require("snacks").picker.lines() end,
-          desc = "Find lines",
-        }
+        maps.n["<Leader>f<CR>"] = { function() p.resume() end, desc = "Resume previous search" }
+        maps.n["<Leader>f'"] = { function() p.marks() end, desc = "Find marks" }
+        maps.n["<Leader>fl"] = { function() p.lines() end, desc = "Find lines" }
         maps.n["<Leader>fa"] = {
-          function() require("snacks").picker.files { cwd = vim.fn.stdpath "config", desc = "Config Files" } end,
+          function() p.files { cwd = vim.fn.stdpath "config", desc = "Config Files" } end,
           desc = "Find AstroNvim config files",
         }
-        maps.n["<Leader>fb"] = { function() require("snacks").picker.buffers() end, desc = "Find buffers" }
-        maps.n["<Leader>fc"] = { function() require("snacks").picker.grep_word() end, desc = "Find word under cursor" }
-        maps.n["<Leader>fC"] = { function() require("snacks").picker.commands() end, desc = "Find commands" }
+        maps.n["<Leader>fb"] = { function() p.buffers() end, desc = "Find buffers" }
+        maps.n["<Leader>fc"] = { function() p.grep_word() end, desc = "Find word under cursor" }
+        maps.n["<Leader>fC"] = { function() p.commands() end, desc = "Find commands" }
         maps.n["<Leader>ff"] = {
-          function()
-            require("snacks").picker.files {
-              hidden = vim.tbl_get((vim.uv or vim.loop).fs_stat ".git" or {}, "type") == "directory",
-            }
-          end,
+          function() p.files { hidden = vim.tbl_get(vim.uv.fs_stat ".git" or {}, "type") == "directory" } end,
           desc = "Find files",
         }
-        maps.n["<Leader>fF"] = {
-          function() require("snacks").picker.files { hidden = true, ignored = true } end,
-          desc = "Find all files",
-        }
-        maps.n["<Leader>fg"] = { function() require("snacks").picker.git_files() end, desc = "Find git files" }
-        maps.n["<Leader>fh"] = { function() require("snacks").picker.help() end, desc = "Find help" }
-        maps.n["<Leader>fk"] = { function() require("snacks").picker.keymaps() end, desc = "Find keymaps" }
-        maps.n["<Leader>fm"] = { function() require("snacks").picker.man() end, desc = "Find man" }
-        maps.n["<Leader>fo"] = { function() require("snacks").picker.recent() end, desc = "Find old files" }
-        maps.n["<Leader>fO"] =
-          { function() require("snacks").picker.recent { cwd = true } end, desc = "Find old files (cwd)" }
-        maps.n["<Leader>fr"] = { function() require("snacks").picker.registers() end, desc = "Find registers" }
-        maps.n["<Leader>fs"] = { function() require("snacks").picker.smart() end, desc = "Find buffers/recent/files" }
-        maps.n["<Leader>ft"] = { function() require("snacks").picker.colorschemes() end, desc = "Find themes" }
-        maps.n["<Leader>fu"] = { function() require("snacks").picker.undo() end, desc = "Undo list" }
+        maps.n["<Leader>fF"] = { function() p.files { hidden = true, ignored = true } end, desc = "Find all files" }
+        maps.n["<Leader>fg"] = { function() p.git_files() end, desc = "Find git files" }
+        maps.n["<Leader>fh"] = { function() p.help() end, desc = "Find help" }
+        maps.n["<Leader>fk"] = { function() p.keymaps() end, desc = "Find keymaps" }
+        maps.n["<Leader>fm"] = { function() p.man() end, desc = "Find man" }
+        maps.n["<Leader>fo"] = { function() p.recent() end, desc = "Find old files" }
+        maps.n["<Leader>fO"] = { function() p.recent { cwd = true } end, desc = "Find old files (cwd)" }
+        maps.n["<Leader>fr"] = { function() p.registers() end, desc = "Find registers" }
+        maps.n["<Leader>fs"] = { function() p.smart() end, desc = "Find buffers/recent/files" }
+        maps.n["<Leader>ft"] = { function() p.colorschemes() end, desc = "Find themes" }
+        maps.n["<Leader>fu"] = { function() p.undo() end, desc = "Undo list" }
         if vim.fn.executable "rg" == 1 then
-          maps.n["<Leader>fw"] = { function() require("snacks").picker.grep() end, desc = "Find words" }
-          maps.n["<Leader>fW"] = {
-            function() require("snacks").picker.grep { hidden = true, ignored = true } end,
-            desc = "Find words in all files",
-          }
+          maps.n["<Leader>fw"] = { function() p.grep() end, desc = "Find words" }
+          maps.n["<Leader>fW"] =
+            { function() p.grep { hidden = true, ignored = true } end, desc = "Find words in all files" }
         end
-        maps.n["<Leader>lD"] = { function() require("snacks").picker.diagnostics() end, desc = "Search diagnostics" }
-        maps.n["<Leader>ls"] = { function() require("snacks").picker.lsp_symbols() end, desc = "Search symbols" }
+        maps.n["<Leader>lD"] = { function() p.diagnostics() end, desc = "Search diagnostics" }
+        maps.n["<Leader>ls"] = { function() p.lsp_symbols() end, desc = "Search symbols" }
 
         maps.n["<Leader>e"] = maps.n["<Leader>ff"]
         maps.n["<space>"] = {
           function()
             if #vim.t.bufs > 1 then
-              require("snacks").picker.buffers {
+              p.buffers {
                 current = false,
                 win = { input = { keys = { ["<Space>"] = { "confirm", mode = { "n", "i" } } } } },
               }
@@ -104,7 +85,7 @@ return {
                     if not package.loaded["todo-comments"] then -- make sure to load todo-comments
                       require("lazy").load { plugins = { "todo-comments.nvim" } }
                     end
-                    require("snacks").picker.todo_comments()
+                    p.todo_comments()
                   end,
                   desc = "Todo Comments",
                 },
@@ -122,7 +103,7 @@ return {
           find_in_dir = function(state)
             local node = state.tree:get_node()
             local path = node.type == "file" and node:get_parent_id() or node:get_id()
-            require("snacks").picker.files { cwd = path }
+            p.files { cwd = path }
           end,
         },
         window = { mappings = { F = "find_in_dir" } },

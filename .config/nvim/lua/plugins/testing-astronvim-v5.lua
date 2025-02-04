ae087d51f411988b128884dacc136ff10ca46434
Glenn Waters <glenn@watrs.ca>
2025-02-03 23:14:18 -0500

    Add config for testing astronvim v5

---
+++ .config/nvim/lua/plugins/testing-astronvim-v5.lua
@@ -0,0 +1,5 @@
+return {
+  { "AstroNvim/astrocore", version = false, branch = "v2" },
+  { "AstroNvim/astrolsp", version = false, branch = "v3" },
+  { "AstroNvim/astroui", version = false, branch = "v3" },
+}

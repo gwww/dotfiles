384e203060c31be91f01980e6163aace96d83e21
Glenn Waters <glenn@watrs.ca>
2025-03-22 21:04:56 -0400

    Update nvim debug fns

--- .config/nvim/lua/debug-helpers.lua
+++ .config/nvim/lua/debug-helpers.lua
@@ -7,6 +7,17 @@ _G.bt = function(...) Debug.backtrace(...) end
 
 Debug.log = {}
 
+local _date = function()
+  local _, ms = vim.uv.gettimeofday()
+  return os.date "%m-%d %H:%M:%S." .. string.format("%.3d ", ms % 1000)
+end
+
+local _prune_log = function()
+  if #Debug.log > 3000 then Debug.log = { unpack(Debug.log, 3000 - 3000 / 4) } end
+end
+
+local _add_to_log = function(prefix, data) table.insert(Debug.log, prefix .. data) end
+
 _G.dbg = function(...)
   local c = select("#", ...)
   local parts = {} ---@type string[]
@@ -16,8 +27,7 @@ _G.dbg = function(...)
   end
 
   local msg = table.concat(parts, " ")
-  local _, ms = vim.uv.gettimeofday()
-  local date = os.date "%m-%d %H:%M:%S." .. string.format("%.3d ", ms % 1000)
+  local date = _date()
   if #msg < 120 then
     table.insert(Debug.log, date .. msg:gsub("%s+", " "))
   else
@@ -27,17 +37,33 @@ _G.dbg = function(...)
       d = "                   "
     end
   end
-  if #Debug.log > 2000 then
-    local note = date .. "(debug log trimmed)"
-    Debug.log = { note, unpack(Debug.log, 1000) }
+  _prune_log()
+end
+
+Debug.backtrace = function(...)
+  if select("#", ...) > 0 then dbg(...) end
+
+  _add_to_log(_date(), "Backtrace...")
+  for level = 3, 20 do
+    local info = debug.getinfo(level, "Sln")
+    if not info then break end
+    if info.what ~= "C" and info.source ~= "lua" then
+      local line = "- " .. vim.fn.fnamemodify(info.source:sub(2), ":p:~:.") .. ":" .. info.currentline
+      if info.name then
+        line = line .. " in " .. info.name .. "()"
+      else
+        line = line .. " in _anonymous_()"
+      end
+      _add_to_log("                   ", line)
+    end
   end
+  _prune_log()
 end
 
 local log_buf_id
 Debug.show_log = function()
   if log_buf_id == nil or not vim.api.nvim_buf_is_valid(log_buf_id) then
     log_buf_id = vim.api.nvim_create_buf(true, true)
-    -- elseif not vim.api.nvim_buf_is_loaded(log_buf_id) then
   end
   vim.api.nvim_set_option_value("buflisted", true, { buf = log_buf_id })
   vim.api.nvim_set_option_value("buftype", "nowrite", { buf = log_buf_id })
@@ -50,24 +76,4 @@ Debug.clear_log = function()
   Debug.log = {}
   vim.cmd 'echo "Cleared log"'
   dbg "Cleared log"
-end
-
-Debug.backtrace = function(...)
-  if select("#", ...) > 0 then dbg(...) end
-
-  local lines = { "Trackback..." }
-  for level = 3, 20 do
-    local info = debug.getinfo(level, "Sln")
-    if not info then break end
-    if info.what ~= "C" and info.source ~= "lua" then
-      local line = "- " .. vim.fn.fnamemodify(info.source:sub(2), ":p:~:.") .. ":" .. info.currentline
-      if info.name then
-        line = line .. " in " .. info.name .. "()"
-      else
-        line = line .. " in _anonymous_()"
-      end
-      table.insert(lines, line)
-    end
-  end
-  dbg(table.concat(lines, "\n"))
 end

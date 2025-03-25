17f55eda0a97106021330dcc8e0d12c0540d1e50
Glenn Waters <glenn@watrs.ca>
2025-03-25 10:29:28 -0400

    Update oil config

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,16 +1,71 @@
 return {
   "stevearc/oil.nvim",
-  ---@module 'oil'
-  ---@type oil.SetupOpts
-  opts = {
-    columns = {
-      "icon",
-      "permissions",
-      "size",
-      "mtime",
+  cmd = "Oil",
+  dependencies = {
+    {
+      "AstroNvim/astrocore",
+      opts = {
+        mappings = {
+          n = {
+            ["<Leader>o"] = { function() require("oil").open() end, desc = "Open folder in Oil" },
+          },
+        },
+        autocmds = {
+          oil_settings = {
+            {
+              event = "FileType",
+              desc = "Disable view saving for oil buffers",
+              pattern = "oil",
+              callback = function(args) vim.b[args.buf].view_activated = false end,
+            },
+            {
+              event = "User",
+              pattern = "OilActionsPost",
+              desc = "Close buffers when files are deleted in Oil",
+              callback = function(args)
+                if args.data.err then return end
+                for _, action in ipairs(args.data.actions) do
+                  if action.type == "delete" then
+                    local _, path = require("oil.util").parse_url(action.url)
+                    local bufnr = vim.fn.bufnr(path)
+                    if bufnr ~= -1 then require("astrocore.buffer").wipe(bufnr, true) end
+                  end
+                end
+              end,
+            },
+          },
+        },
+      },
+    },
+    {
+      "rebelot/heirline.nvim",
+      optional = true,
+      dependencies = { "AstroNvim/astroui", opts = { status = { winbar = { enabled = { filetype = { "^oil$" } } } } } },
+      opts = function(_, opts)
+        if opts.winbar then
+          local status = require "astroui.status"
+          table.insert(opts.winbar, 1, {
+            condition = function(self) return status.condition.buffer_matches({ filetype = "^oil$" }, self.bufnr) end,
+            status.component.separated_path {
+              padding = { left = 2 },
+              max_depth = false,
+              suffix = false,
+              path_func = function(self) return require("oil").get_current_dir(self.bufnr) end,
+            },
+          })
+        end
+      end,
     },
   },
-  -- Optional dependencies
-  -- dependencies = { { "echasnovski/mini.icons", opts = {} } },
-  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+  opts = function()
+    local get_icon = require("astroui").get_icon
+    return {
+      columns = {
+        { "icon", default_file = get_icon "DefaultFile", directory = get_icon "FolderClosed" },
+        "permissions",
+        "size",
+        "mtime",
+      },
+    }
+  end,
 }

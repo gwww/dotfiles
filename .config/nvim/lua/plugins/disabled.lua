4a7b8aa82d42c04df0b3203b525bb40aaa720ec5
Glenn Waters <glenn@watrs.ca>
2025-03-26 21:32:24 -0400

    Tidy

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -1,8 +1,6 @@
 -- Disabled plugins
 return {
   { "rcarriga/nvim-dap-ui", enabled = false },
-  { "goolord/alpha-nvim", enabled = false },
-  { "lukas-reineke/indent-blankline.nvim", enabled = false },
   { "AstroNvim/astrotheme", enabled = false },
   { "RRethy/vim-illuminate", enabled = false },
   { "max397574/better-escape.nvim", enabled = false },

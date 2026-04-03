efe42f71a8afeaece75b31da346e50e5a8705a52
Glenn Waters <glenn@watrs.ca>
2026-04-03 13:24:56 -0400

    Bump astronvim to v6 for nvim 0.12

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,14 +3,17 @@
 local lazypath = vim.env.LAZY or vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 if not (vim.env.LAZY or vim.uv.fs_stat(lazypath)) then
   -- stylua: ignore
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable",
+    lazypath })
 end
 vim.opt.rtp:prepend(lazypath)
 
 local lazy_loaded, lazy = pcall(require, "lazy")
 if not lazy_loaded then
   -- stylua: ignore
-  vim.api.nvim_echo({ { ("Unable to load lazy from: %s\n"):format(lazypath), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } }, true, {})
+  vim.api.nvim_echo(
+    { { ("Unable to load lazy from: %s\n"):format(lazypath), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } },
+    true, {})
   vim.fn.getchar()
   vim.cmd.quit()
 end
@@ -19,11 +22,11 @@ pcall(require, "debug-helpers")
 pcall(require, "pre-setup")
 
 lazy.setup({
-  -- { "AstroNvim/AstroNvim", version = "^5", import = "astronvim.plugins" },
-  { "AstroNvim/AstroNvim", branch = "v6", import = "astronvim.plugins" },
-  { "AstroNvim/astrolsp", branch = "v4" },
-  { "AstroNvim/astrocore", branch = "v3" },
-  { "AstroNvim/astroui", branch = "v4" },
+  { "AstroNvim/AstroNvim", version = "^6", import = "astronvim.plugins" },
+  -- { "AstroNvim/AstroNvim", branch = "v6", import = "astronvim.plugins" },
+  -- { "AstroNvim/astrolsp", branch = "v4" },
+  -- { "AstroNvim/astrocore", branch = "v3" },
+  -- { "AstroNvim/astroui", branch = "v4" },
 
   { import = "plugins" },
 }, {

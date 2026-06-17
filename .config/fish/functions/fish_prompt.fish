c072ad8e1f6191f4258452cb32fb4504c01d90e9
Glenn Waters <glenn@watrs.ca>
2026-06-16 20:13:11 -0400

    Fix fish prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,5 +1,7 @@
 function fish_prompt --description 'Write out the prompt'
-  test $status -ne 0; and set -l c (set_color red); or set -l c (set_color green)
+  set -l last_status $status
+  set -l c (test $last_status -eq 0; and set_color green; or set_color red)
+
   fish_is_root_user; and set -l prompt_char $c'#'; or set -l prompt_char $c'λ'
 
   test -n "$SSH_CLIENT" -o -n "$SSH_TTY"; and echo -n (hostname -s)' '
@@ -8,22 +10,28 @@ function fish_prompt --description 'Write out the prompt'
   # Python VENV
   test -n "$VIRTUAL_ENV"; and echo -n (set_color 00d7ff) (string split -r -m 2 -f 2 '/' $VIRTUAL_ENV)' '
 
+  # Context: High-Performance Git Porcelain v2 Parse
   set -l git_status (git status --branch --porcelain=2 2> /dev/null)
   if test -n "$git_status"
-    set -l branch (string match -r '^# branch.head (.*)' $git_status)[2]
-    set -l c "brmagenta"
-    set -l updown ""
-    if [ $branch = "(detached)" ]
-      set branch (string match -r '^# branch.oid (.......)' $git_status)[2]
-    else
-      set updown (string match -r '^# branch.ab [+-]?([\d]+) [+-]?([\d]+)' $git_status)
-      if test $status -eq 0 -a "$updown[2] $updown[3]" != "0 0"
-        echo -n (set_color bryellow) $updown[2]$updown[3]' '
-      end
-      test -z "$(string match -r '^[^#]' $git_status)"; and set c "green"; or set c "red"
-    end
-    echo -n (set_color $c) $branch' '
-  end
+    set -l branch_color
 
+    set -l branch (string match -r '^# branch.head (.*)' $git_status)[2]
+    if test "$branch" = "(detached)"
+      set branch (string match -r '^# branch.oid (.......)' $git_status)[2]
+      set branch_color "brmagenta"
+    else
+      set -l updown (string match -rg '^# branch.ab \+([\d]+) \-([\d]+)' $git_status)
+      if test (count $updown) -eq 2; and test "$updown[1] $updown[2]" != "0 0"
+        echo -n (set_color bryellow) $updown[1]$updown[2]' '
+      end
+            
+      if string match -rq '^[^#]' $git_status
+        set branch_color "red"
+      else
+        set branch_color "green"
+      end
+    end
+    echo -n (set_color $branch_color) $branch' '
+  end
   echo -n $prompt_char (set_color normal)
 end

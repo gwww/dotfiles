7746f4af4ff6b3db1e2ef85c6a37983c8d7be7a4
Glenn Waters <glenn@watrs.ca>
2026-07-18 19:56:37 -0400

    Switch to using hs.hotkey modal fn vs homebrew

--- .config/hammerspoon2/lib/hyper.js
+++ .config/hammerspoon2/lib/hyper.js
@@ -3,27 +3,32 @@ class Hyper {
     this.presses = 0
     this.pressHooks = []
     this.releaseHooks = []
-    this.modal = new Modal()
+    this._actions = []
 
-    this.hyperHotkey = hs.hotkey.bind([], modalKey, () => {
+    this.modalPressed = () => {
       this.presses = 0
       this.modal.enter()
       this.pressHooks.forEach(hook => hook())
-    }, () => {
+    }
+    this.modalReleased = () => {
       this.modal.exit()
       this.releaseHooks.forEach(hook => hook(this.presses))
-    })
+    }
+
+    this.modal = hs.hotkey.createModal([], '')
+    this._modalKey = hs.hotkey.bind([], modalKey, this.modalPressed, this.modalReleased)
   }
 
   bind(bindings) {
     const list = Array.isArray(bindings) ? bindings : [bindings]
     list.forEach(binding => {
       const mods = binding.modifiers || []
-      const action = () => {
+      const fn = () => {
         this.presses++
         binding.action()
       }
-      this.modal.bind(mods, binding.key, action)
+      this._actions.push(fn) // Prevents GC
+      this.modal.bind(mods, binding.key, fn, null)
     })
     return this
   }
